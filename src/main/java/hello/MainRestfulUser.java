8844a1ae788cd95b52b66f8efcf517609525d400
lconstantin <ctin.laura93@gmail.com>
2016-08-09 18:23:19 +0300

    last commit

--- src/main/java/hello/MainRestfulUser.java
+++ src/main/java/hello/MainRestfulUser.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {EmbeddedServletContainerAutoConfiguration.class, WebMvcAutoConfiguration.class})
 
-public class SpringbootResttemplateApplication {
+public class MainRestfulUser {
     protected static final String ACCESS_TOKEN = "EAACEdEose0cBAMGGv3mkZAHLBAI3mFuabFsKc02zyfxbypeCCZCBGseaJy8hpCDsOXKB838vpvmo3m0chrJdZBma74WVJR22mkL87UVChsUjYrmDoZB5DKmGBgOR8Bx1rF06B5YGVWkJVhIUz7BIZBIq7Ng5jsS8r7RzOpeVtLAZDZD";
     private static final String API_VERSION = "2.7";
     private static final String GRAPH_API_URL = "https://graph.facebook.com/v" + API_VERSION + "/me?access_token=" + ACCESS_TOKEN;
@@ -20,7 +20,6 @@ public class SpringbootResttemplateApplication {
     public static void main(String[] args) {
 
         RestTemplate restTemplate = new RestTemplate();
-        RestfulUser restfulUser = new RestfulUser();
 
         User user = restTemplate.getForObject(GRAPH_API_URL,User.class,200);
         System.out.println("ID: " + user.getId());
--- src/test/java/FacebookTest.java
+++ src/test/java/FacebookTest.java
@@ -1,6 +1,5 @@
-import hello.RestfulUser;
+import hello.User;
 import org.junit.Test;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -11,13 +10,11 @@ public class FacebookTest {
 
     @Test
     public void convert_json_response_to_java_obj() {
+        RestTemplate restTemplate = new RestTemplate();
 
-       RestfulUser restfulUser = new RestfulUser();
-      //  restfulUser.getEntity();
-//        JSONObject json = JSONObject.fromObject(res.getBody());
-//        String token = json.getString(ACCESS_TOKEN);
-
-
+        User user = restTemplate.getForObject(GRAPH_API_URL,User.class,200);
+        System.out.println("ID: " + user.getId());
+        System.out.println("ID: " + user.getName());
 
     }
 }
